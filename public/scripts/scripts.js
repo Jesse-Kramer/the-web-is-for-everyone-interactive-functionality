2c99e533f6ded5f30beb258a0ea2590652649e56
Jesse Kramer <jessekramer15@gmail.com>
2024-04-15 10:27:50 +0200

    Navbar added

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -14,3 +14,34 @@ function shareButton(){
         // For example, you can open a dialog box with the link for manual sharing
     }
 }
+
+/* Hamburger menu code*/
+
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const menuIcon = document.querySelector('.menu_icon');
+    if (menuIcon) {
+        const burgerMenu = document.querySelector('.burger_menu_window');
+        const closeIcon = document.querySelector('.close_icon');
+
+        menuIcon.addEventListener('click', function () {
+            // Change translate position of menu icon
+            menuIcon.style.transform = 'translateX(-200%)';
+            // Show burger menu
+            burgerMenu.style.transform = 'translateX(0)';
+            // Hide menu icon
+            menuIcon.hidden = true;
+        });
+
+        closeIcon.addEventListener('click', function () {
+            // Change translate position of menu icon back to default
+            menuIcon.style.transform = 'translateX(0)';
+            // Hide burger menu
+            burgerMenu.style.transform = 'translateX(-200%)';
+            // Show menu icon
+            menuIcon.hidden = false;
+        });
+
+    }
+});
